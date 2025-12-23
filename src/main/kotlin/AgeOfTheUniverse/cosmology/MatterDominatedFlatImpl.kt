2362948100957114b75a079ec111de16fbc3d1d3
TatsukiKodama8 <kodama_tatsuki@retail-ai.jp>
2025-12-23 09:13:21 +0900

    chore: commentを編集

--- src/main/kotlin/AgeOfTheUniverse/cosmology/MatterDominatedFlatImpl.kt
+++ src/main/kotlin/AgeOfTheUniverse/cosmology/MatterDominatedFlatImpl.kt
@@ -3,12 +3,14 @@ package AgeOfTheUniverse.cosmology
 import kotlin.math.sqrt
 import kotlin.math.pow
 
+/**
+ * Matter-dominated flat model implementation.
+ */
 class MatterDominatedFlatImpl : CosmologyModelStrategy {
-    override val name = "Matter Dominated Flat (Einstein-de Sitter)"
+    override val name = "Matter Dominated Flat"
 
     override fun E(a: Double): Double {
         if (a <= 0.0) return Double.MAX_VALUE
-        // Flat, Matter only: Omega_m = 1
         return sqrt(a.pow(-3))
     }
 }
